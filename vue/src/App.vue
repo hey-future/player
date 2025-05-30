55496dbd822f7ac8f4ab619a07ead5deb6ced3c6
wangmanna <wangmanna@dayu-cloud.com>
2025-05-30 15:33:30 +0800

    '播放器demo修改'

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -27,6 +27,8 @@
   <button @click="handleInit(1)">初始化</button>
   <h3> 播放器方法 </h3>
   <button @click="handleDestory">销毁</button>
+  <button @click="showProgressMarkers">显示视频打点标记</button>
+  <button @click="hideProgressMarkers">隐藏视频打点标记</button>
   <button @click="hanldeShowKnowledge(true)">显示知识点开关</button>
   <button @click="hanldeShowKnowledge(false)">隐藏知识点开关</button>
   <button @click="changeKnowledgeOpen(true)">打开知识点显示</button>
@@ -34,6 +36,8 @@
   <button @click="handleSetViewPoints()">设置观看记录进度条</button>
   <button @click="handleChangeViewPointsShow(true)">显示观看记录进度条</button>
   <button @click="handleChangeViewPointsShow(false)">隐藏观看记录进度条</button>  
+  <button @click="initEnterExitPoint([0,300])">开启打点标记功能</button>
+  <button @click="initEnterExitPoint()">关闭打点标记功能</button> 
 </template>
 <script lang="ts" setup>
 import { nextTick, reactive, ref } from "vue";
@@ -64,45 +68,7 @@ const handleSetViewPoints = () => {
   videoRef.value.setViewPoints(arrPoint);
 }
 const hanldeShowKnowledge = (val) => {
-  videoRef.value.changeKnowledgeShow(val);
-}
-const changeKnowledgeOpen = (val) => {
-  videoRef.value.changeKnowledgeOpen(val);
-
-}
-const option = reactive({
-  appId: 18,
-  vid: 211370,
-  playType: 2,
-  url: 'https://vjs.zencdn.net/v/oceans.mp4',
-  enterExitPoint: [170.72, 382.76],
-});
-const progressInfo = ref([]);
-for (let i = 0; i < 100; i++) {
-  progressInfo.value.push(i);
-}
-for (let i = 200; i < 500; i++) {
-  progressInfo.value.push(i);
-}
-const options = {
-  cover: "https://oucp.multimediapress.cn//images/1/2022/07/28/cdc35935-a46d-42db-b664-6b8eb4db9d85_1.jpg",
-  logo: [
-    {
-      width: 20,
-      position: "top-left",
-      origin: "box",
-      src: "./static/images/player_logo.png",
-      offsetY: 1,
-      offsetX: 2,
-    }
-  ],
-  // disableSeek:true, //禁用进度条
-  autoPlay: true,
-  // mute: true,
-  volume: 1,
-  knowledgeOpen: true,
-  // viewPoints: progressInfo,
-  knowledge: [
+  const knowledge = [
     {
       name: "资本积累的本质与形式",
       summary: "资本家通过剩余价值资本化实现规模扩张，本质是剥削关系的再生产。",
@@ -131,8 +97,22 @@ const options = {
         "剩余价值转化为利润掩盖剥削，资本主义基本矛盾导致周期性经济危机。",
       fromTo: [1272.14, 1873.29],
     },
-  ],
-  marks: [
+  ]
+  videoRef.value.changeKnowledgeShow(val,knowledge);
+}
+const changeKnowledgeOpen = (val) => {
+  videoRef.value.changeKnowledgeOpen(val);
+}
+
+const initEnterExitPoint = (data?) => {
+  videoRef.value.initEnterExitPoint(data);
+}
+
+const hideProgressMarkers = () => {
+  videoRef.value.setProgressMarkers([]);
+}
+const showProgressMarkers = () => {
+  const marks = [
     {
       text: "资本积累的本质与形式",
       offset: 24.3,
@@ -157,8 +137,42 @@ const options = {
       text: "剩余价值理论与经济危机",
       offset: 1272.14,
       time: 1272.14,
-    },
+    }
+  ]
+  videoRef.value.setProgressMarkers(marks);
+}
+const option = reactive({
+  appId: 18,
+  vid: 211370,
+  playType: 2,
+  url: 'https://vjs.zencdn.net/v/oceans.mp4'
+});
+const progressInfo = ref([]);
+for (let i = 0; i < 100; i++) {
+  progressInfo.value.push(i);
+}
+for (let i = 200; i < 500; i++) {
+  progressInfo.value.push(i);
+}
+const options = {
+  cover: "https://oucp.multimediapress.cn//images/1/2022/07/28/cdc35935-a46d-42db-b664-6b8eb4db9d85_1.jpg",
+  logo: [
+    {
+      width: 20,
+      position: "top-left",
+      origin: "box",
+      src: "./static/images/player_logo.png",
+      offsetY: 1,
+      offsetX: 2,
+    }
   ],
+  // disableSeek:true, //禁用进度条
+  autoPlay: true,
+  // mute: true,
+  volume: 1,
+  knowledgeOpen: true,
+  // viewPoints: progressInfo,
+
   speed: 1, // 播放速度
   speedLevels: [
     { value: 0.25, label: "0.25x" },
@@ -191,6 +205,7 @@ const options = {
   disablePip: true
 };
 
+
 const handleLogin = () => {};
 const handleTimeUpdate = (data) => {
   // console.log(data)
@@ -204,16 +219,15 @@ const handleTimeUpdate = (data) => {
   height: 320px;
 }
 label {
-  margin: 0 16px;
+  margin: 8px 16px;
 }
 input {
-  margin: 0 8px;
+  margin:8px;
   height: 30px;
 }
 button {
   height: 32px;
+  margin: 8px;
 }
-button+button {
-  margin-left: 16px;
-}
+
 </style>
